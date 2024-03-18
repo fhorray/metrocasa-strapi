cfc51a43d4003fb9c2e72d9e67b2d746f6e05c5f
Francy Santos <francy.horray@gmail.com>
2024-03-17 23:43:30 -0300

    feat: add empreendimentos details

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,65 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface FacilitiesFacility extends Schema.Component {
+  collectionName: 'components_facilities_facilities';
+  info: {
+    displayName: 'facility';
+    description: '';
+  };
+  attributes: {
+    facility: Attribute.Enumeration<
+      [
+        'Academia',
+        'Biciclet\u00E1rio',
+        'Brinquedoteca',
+        'Churrasqueira',
+        'Churrasqueira com Pergolado',
+        'Coworking',
+        'Delivery Space',
+        'Ducha',
+        'Espa\u00E7o com Espelho D\u2019\u00E1gua',
+        'Espa\u00E7o Cross Training',
+        'Espa\u00E7o Gourmet',
+        'Espa\u00E7o Kids',
+        'Espa\u00E7o Leitura',
+        'Espa\u00E7o Multiuso',
+        'Espa\u00E7o Pet Care',
+        'Espa\u00E7o Sport Bar',
+        'Espa\u00E7o Zen',
+        'Fitness Externo',
+        'Hall de Acesso',
+        'Hall Social',
+        'Lavanderia',
+        'Living',
+        'Lobby',
+        'Lounge',
+        'Market',
+        'Paisagismo',
+        'Patinete Place',
+        'Pet Place',
+        'Piscina',
+        'Playground',
+        'Portaria',
+        'Portaria com Clausura',
+        'Pra\u00E7a da Fogueira',
+        'Pra\u00E7a de Conviv\u00EAncia',
+        'Pra\u00E7a de Leitura',
+        'Quadra Poliesportiva',
+        'Quadra Recreativa',
+        'Red\u00E1rio',
+        'Sala de Estudos',
+        'Sal\u00E3o de Festas',
+        'Sal\u00E3o de Jogos',
+        'Sauna',
+        'Sky Lounge',
+        'Sol\u00E1rio',
+        'Spa / Sauna',
+        'Sport Play'
+      ]
+    >;
+  };
+}
+
 export interface PlantasPlanta extends Schema.Component {
   collectionName: 'components_plantas_plantas';
   info: {
@@ -27,6 +87,7 @@ export interface PlantasPlanta extends Schema.Component {
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'facilities.facility': FacilitiesFacility;
       'plantas.planta': PlantasPlanta;
     }
   }
