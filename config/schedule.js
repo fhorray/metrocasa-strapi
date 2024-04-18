dfa0d77844cc12e21a396a28735b5f157c019bee
Francy Santos <francy.horray@gmail.com>
2024-04-18 18:51:08 -0300

    feat: initial schedule

---
+++ config/schedule.js
@@ -0,0 +1,11 @@
+module.exports = {
+  schedule: {
+    tasks: [
+      {
+        task: "delete-old-users",
+        cron: "0 0 * * *", // Executa a tarefa todos os dias às 00:00
+        enabled: true,
+      },
+    ],
+  },
+};
