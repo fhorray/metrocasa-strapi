3d470a79e7788816d8f2f1a0251f28a654997153
Francy Santos <francy.horray@gmail.com>
2024-05-27 13:48:42 -0300

    files adjustments

--- config/middlewares.js
+++ config/middlewares.js
@@ -26,6 +26,14 @@ module.exports = [
             // "yourBucketName.s3.yourRegion.amazonaws.com"
             "cdn-metrocasa.s3.us-east-1.amazonaws.com",
           ],
+          "pdf-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "cdn-metrocasa.s3.us-east-1.amazonaws.com",
+          ],
+
           upgradeInsecureRequests: null,
         },
       },
@@ -34,7 +42,22 @@ module.exports = [
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
-  "strapi::body",
+  {
+    name: "strapi::body",
+    config: {
+      parserOptions: {
+        jsonLimit: "256mb",
+        formLimit: "256mb",
+        textLimit: "256mb",
+      },
+      formLimit: "256mb",
+      jsonLimit: "256mb",
+      textLimit: "256mb",
+      formidable: {
+        maxFileSize: 250 * 1024 * 1024,
+      },
+    },
+  },
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
